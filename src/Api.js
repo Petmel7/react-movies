e98c30d725ab0d5f56ff8abc59c529bc19f6751b
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-09-18 14:05:27 +0300

    add

--- src/Api.js
+++ src/Api.js
@@ -15,26 +15,58 @@
 //     return response.data;
 // }
 
-import axios from "axios";
+// import axios from "axios";
 
-// Встановлюємо заголовки для всіх запитів
-axios.defaults.headers.common["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA9MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48';
+// // Встановлюємо заголовки для всіх запитів
+// axios.defaults.headers.common["Authorization"] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA9MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48';
 
-// Встановлюємо базовий URL
-const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/eb4ad2dbf591c1e36f713d73c2092c42",
-});
+// // Встановлюємо базовий URL
+// const api = axios.create({
+//   baseURL: "https://api.themoviedb.org/3/eb4ad2dbf591c1e36f713d73c2092c42",
+// });
 
-export const fetchMovies = async () => {
-  try {
-    const response = await api.get("/movies", {
-      params: {
-        // тут можна додати додаткові параметри запиту, якщо вони потрібні
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Помилка при отриманні фільмів:", error);
-    throw error; // Кидаємо помилку, щоб обробити її вище, якщо потрібно
+// export const fetchMovies = async () => {
+//   try {
+//     const response = await api.get("/movies", {
+//       params: {
+//         // тут можна додати додаткові параметри запиту, якщо вони потрібні
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     console.error("Помилка при отриманні фільмів:", error);
+//     throw error; // Кидаємо помилку, щоб обробити її вище, якщо потрібно
+//   }
+// };
+
+
+import axios from 'axios';
+
+const options = {
+  
+  url: 'https://api.themoviedb.org/3/discover/movie',
+  params: {
+    
+    language: 'en-US',
+    page: '1',
+    
+  },
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA5MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48'
   }
 };
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+  export const fetchMovies = async () => {
+    const response = await axios.get('/movies');
+    return response.data;
+}
