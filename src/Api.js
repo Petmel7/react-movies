729d98f33476621897b068475435b5c543d9aa9b
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-09-24 19:19:38 +0300

    add

--- src/Api.js
+++ src/Api.js
@@ -11,8 +11,8 @@ export const axiosMovies = async () => {
   return response.data;
 };
 
-// export const axiosMoviesSearch = async () => {
-//   const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
+// export const axiosMoviesSearch = async value => {
+//   const response = await axios.get(`https://api.themoviedb.org/3/search/movie/?${value}`, {
 //     // params: {include_adult: 'false', language: 'en-US', page: '1'},
 //     headers: {
 //       accept: 'application/json',
@@ -40,24 +40,24 @@ export const axiosMovies = async () => {
 //   return response.data;
 // };
 
-const options = {
-  method: 'GET',
-  url: 'https://api.themoviedb.org/3/search/movie',
-  params: {include_adult: 'false', language: 'en-US', page: '1'},
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA5MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.themoviedb.org/3/search/movie',
+//   params: {include_adult: 'false', language: 'en-US', page: '1'},
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA5MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48'
+//   }
+// };
 
-axios
-  .request(options)
-  .then(function (response) {
-    console.log('', response.data);
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log('', response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
 export const axiosMovieId = async movieId => {
   const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -93,47 +93,158 @@
 // const rootElement = document.getElementById('root');
 // ReactDOM.render(<App />, rootElement);
 
-import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { createRoot } from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import { Formik, Field, Form } from 'formik';
-import { axiosMoviesSearch } from '../../Api';
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+// // import { createRoot } from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+// import { Formik, Field, Form } from 'formik';
+// import { axiosMoviesSearch } from '../../Api';
 
-export const MoviesPage = () => (
+// export const MoviesPage = () => (
 
-    <div>
-        <h1>MoviesPage</h1>
-        <Formik
-            initialValues={{
-                movieName: '',
-            }}
-            onSubmit={async (movieName) => {
-                // await new Promise((r) => setTimeout(r, 500));
-                // alert(JSON.stringify(values, null, 2));
-                try {
-                    const search = await axiosMoviesSearch(movieName);
-                    console.log('search', search);
-                    // alert(JSON.stringify(values, null, 2));
-                    console.log('values', movieName)
-                } catch (error) {
-                    console.log('Error Search', error);
+//     <div>
+//         <h1>MoviesPage</h1>
+//         <Formik
+//             initialValues={{
+//                 movieName: '',
+//             }}
+//             onSubmit={async (movieName) => {
+//                 // await new Promise((r) => setTimeout(r, 500));
+//                 // alert(JSON.stringify(values, null, 2));
+//                 // try {
+//                 //     const search = await axiosMoviesSearch(movieName);
+//                 //     console.log('search', search);
+//                 //     // alert(JSON.stringify(values, null, 2));
+//                 //     console.log('values', movieName)
+//                 // } catch (error) {
+//                 //     console.log('Error Search', error);
+//                 // }
+//             }}
+//         >
+//             <Form>
+//                 <label htmlFor="movieName"></label>
+//                 <Field id="movieName" name="movieName" placeholder="Search movies" />
+
+//                 <button type="submit">Submit</button>
+//             </Form>
+//         </Formik>
+//     </div>
+// );
+
+// // ReactDOM.render(<MoviesPage />, document.getElementById('root'));
+
+// const root = document.getElementById('root');
+// const reactRoot = createRoot(root);
+
+// reactRoot.render(<MoviesPage />);
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Formik, Form, Field } from 'formik';
+
+export const MoviesPage = () => {
+    const [movies, setMovies] = useState([]);
+    const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA5MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48';
+
+    const handleSearch = async (query) => {
+        try {
+            const response = await axios.get(
+                `https://api.themoviedb.org/3/search/movie?query=${query}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${apiKey}`,
+                    },
                 }
-            }}
-        >
-            <Form>
-                <label htmlFor="movieName"></label>
-                <Field id="movieName" name="movieName" placeholder="Search movies" />
+            );
+            const movieResults = response.data.results;
+            console.log(movieResults)
+            setMovies(movieResults);
+        } catch (error) {
+            console.error('Error searching for movies:', error);
+        }
+    };
 
-                <button type="submit">Submit</button>
-            </Form>
-        </Formik>
-    </div>
-);
+    return (
+        <div>
+            <h1>Movies Page</h1>
+            <Formik initialValues={{ movieName: '' }} onSubmit={handleSearch}>
+                <Form>
+                    <Field
+                        type="text"
+                        name="movieName"
+                        placeholder="Search movies..."
+                    />
+                    <button type="submit">Search</button>
+                </Form>
+            </Formik>
 
-// ReactDOM.render(<MoviesPage />, document.getElementById('root'));
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>{movie.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-const root = document.getElementById('root');
-const reactRoot = createRoot(root);
 
-reactRoot.render(<MoviesPage />);
+
+
+
+
+// import { useState, useEffect } from 'react';
+// // import axios from 'axios';
+// import { Formik, Form, Field } from 'formik';
+// import { axiosMoviesSearch } from '../../Api';
+
+// export const MoviesPage = () => {
+//     const [movies, setMovies] = useState([]);
+//     // const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjRhZDJkYmY1OTFjMWUzNmY3MTNkNzNjMjA5MmM0MiIsInN1YiI6IjY1MDZlODlmM2NkMTJjMDEyZGU3ZmEwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.R26ObZZn8eNnjk4cSDh5BZY8D3wGyztliY6hEkVyn48';
+
+//     useEffect(() => {
+//         const handleSearch = async () => {
+//         try {
+//             // const response = await axios.get(
+//             //     `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${values.movieName}`
+//             // );
+
+//             // const response = await axios.get(
+//             //     `https://api.themoviedb.org/3/search/movie?query=${value}`,
+//             //     {
+//             //         headers: {
+//             //             Authorization: `Bearer ${apiKey}`,
+//             //         },
+//             //     }
+//             // );
+//             const movieResults = await axiosMoviesSearch(value);
+//             console.log(movieResults)
+//             setMovies(movieResults);
+//         } catch (error) {
+//             console.error('Error searching for movies:', error);
+//         }
+//         };
+//         handleSearch()
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1>Movies Page</h1>
+//             <Formik initialValues={{ movieName: '' }} onSubmit={handleSearch}>
+//                 <Form>
+//                     <Field
+//                         type="text"
+//                         name="movieName"
+//                         placeholder="Search movies..."
+//                     />
+//                     <button type="submit">Search</button>
+//                 </Form>
+//             </Formik>
+
+//             <ul>
+//                 {movies.map((movie) => (
+//                     <li key={movie.id}>{movie.title}</li>
+//                 ))}
+//             </ul>
+//         </div>
+//     );
+// };
