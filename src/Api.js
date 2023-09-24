68bc8f2709b5fa4df8fc53e39d99dd7202fadb42
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-09-24 11:15:57 +0300

    add

--- src/Api.js
+++ src/Api.js
@@ -53,7 +53,7 @@ const options = {
 axios
   .request(options)
   .then(function (response) {
-    console.log(response.data);
+    console.log('', response.data);
   })
   .catch(function (error) {
     console.error(error);
