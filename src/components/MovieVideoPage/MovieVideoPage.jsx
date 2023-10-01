6be97a5deb7a7e710122c233f64ce8fe4dbba26d
Petmel7 <115225282+Petmel7@users.noreply.github.com>
2023-10-01 14:25:15 +0300

    add

--- src/components/MovieVideoPage/MovieVideoPage.jsx
+++ src/components/MovieVideoPage/MovieVideoPage.jsx
@@ -5,7 +5,7 @@ import { axiosMovieVideos } from '../../Api';
 export const MovieVideoPage = () => {
     const { movieId } = useParams();
     const [movieVideos, setMovieVideos] = useState(null);
-    console.log(movieVideos)
+    console.log('movieVideos', movieVideos)
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -28,16 +28,43 @@ export const MovieVideoPage = () => {
                 loading ? (
                     <h1> Loading...</h1>
                 ) : (
-                    <ul>
-                        <video controls width="400" height="300">
-                            {movieVideos.results.map((result) => (
-                                <li>
-                                    <source src={result.official} type="video/mp4" />
-                                </li>
-                            ))}
-                            Ваш браузер не підтримує відтворення відео.
-                        </video>
-                    </ul>
+                    movieVideos && (
+                        // <video controls width="400" height="300">
+                        //     {movieVideos.results.map((video) => (
+                        //         <source src={video.official.toString()} type="video" key={video.id} />
+                        //     ))}
+                        //     Ваш браузер не підтримує відтворення відео.
+                        // </video>
+                        <div>
+                            {/* {movieVideos.results.map((video) => (
+                                <div key={video.id}>
+                                    <video controls width="400" height="300">
+                                        <source src={video.official.toString()} type="video" />
+                                        Ваш браузер не підтримує відтворення відео.
+                                    </video>
+                                    <div>{video.name}</div>
+                                    <div>{video.published_at}</div>
+                                    <div>{video.site}</div>
+                                    <div>{video.size}</div>
+                                    <div>{video.type}</div>
+                                </div>
+                            ))} */}
+                                
+
+                                
+                                <div >
+                                    {/* <video controls width="400" height="300">
+                                        <source src={`https://video.tmdb.org/t/p/w400${movieVideos.results.official}`} type="video" />
+                                        Ваш браузер не підтримує відтворення відео.
+                                    </video> */}
+                                    <div>{movieVideos.results.name}</div>
+                                    <div>{movieVideos.results.published_at}</div>
+                                    <div>{movieVideos.results.site}</div>
+                                    <div>{movieVideos.results.size}</div>
+                                    <div>{movieVideos.results.type}</div>
+                                </div>
+                        </div>
+                    )
                 )
             }
         </div>
